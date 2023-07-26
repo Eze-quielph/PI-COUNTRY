4472eb86b1343a38950857502d828cfbeea0a26e
Ezequiel Benitez <125520493+Eze-quielph@users.noreply.github.com>
2023-07-26 11:45:07 -0300

    fix server/serverjs change app of server

--- server/src/server.js
+++ server/src/server.js
@@ -8,7 +8,7 @@ const server = express();
 server.use(morgan("dev"));
 server.use(express.json());
 // Configurar cabeceras y cors
-app.use((req, res, next) => {
+server.use((req, res, next) => {
     res.header('Access-Control-Allow-Origin', '*');
     res.header('Access-Control-Allow-Headers', 'Authorization, X-API-KEY, Origin, X-Requested-With, Content-Type, Accept, Access-Control-Allow-Request-Method');
     res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, DELETE');
